e500889ced951c872d7ce35a10f54a09393a7c32
Natnael Desalegnh <c21ndh@cs.umu.se>
2025-04-25 13:20:38 +0200

    David fuck you

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -6,18 +6,33 @@ public class TestApp {
      * TestApp without branching statements
      */
     public void test1() {
+        int i = 0;
+        int a = 6;
+        System.out.println("a + i = " + a + i);
+
     }
 
     /**
      * TestApp with if-else branching statements
      */
     public void test2() {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
     }
 
     /**
      * TestApp with looping statement
      */
     public void test3() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
     }
 
 
