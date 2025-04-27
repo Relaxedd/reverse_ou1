9b17121c06974202c110cac67edf567c5886910e
David <david.olofsson02@gmail.com>
2025-04-27 12:47:14 +0200

    added prints for a question

--- src/main/java/org/ses/re/assignment1/analysis/Main.java
+++ src/main/java/org/ses/re/assignment1/analysis/Main.java
@@ -2,6 +2,8 @@ package org.ses.re.assignment1.analysis;
 
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.jimple.common.stmt.JAssignStmt;
+import sootup.core.jimple.common.stmt.JReturnStmt;
+import sootup.core.jimple.common.stmt.JReturnVoidStmt;
 import sootup.core.jimple.common.stmt.Stmt;
 import sootup.core.model.SootClass;
 import sootup.core.model.SootMethod;
@@ -42,6 +44,8 @@ public class Main {
         View view = new JavaView(analysisInputLocations);
 
         int totalDefinitionsCnt = 0;
+        int returnCnt = 0;
+        int returnVoidCnt = 0;
 
 
         for (SootClass clazz : view.getClasses()) {
@@ -50,11 +54,16 @@ public class Main {
                 for (Stmt d : method.getBody().getStmts()) {
                     if (d instanceof JAssignStmt) {
                         totalDefinitionsCnt += analysis.getFlowAfter(d).size();
+                    } else if (d instanceof JReturnStmt) {
+                        returnCnt++;
+                    } else if (d instanceof JReturnVoidStmt) {
+                        returnVoidCnt++;
                     }
                 }
             }
         }
-
+        System.out.println("Return Statements: " + returnCnt);
+        System.out.println("Return Void Statements: " + returnVoidCnt);
         System.out.println("Reachable Definitions: " + totalDefinitionsCnt);
     }
 }
